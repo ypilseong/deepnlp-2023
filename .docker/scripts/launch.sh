09ac8967e4e0fc0f3435ac835bbb3e615cf4f073
Young Joon Lee <entelecheia@hotmail.com>
2023-10-21 10:08:53 +0900

    refactor(docker): simplify startup process, add jupyterlab installation

--- .docker/scripts/launch.sh
+++ .docker/scripts/launch.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 # add your custom commands here that should be executed every time the docker container starts
+
+# print environment variables
+export
+
 echo "Starting docker container..."
-
-### Set the USER_UID envvar to match your user.
-# Ensures files created in the container are owned by you:
-#   docker run --rm -it -v /some/path:/invokeai -e USER_UID=$(id -u) <this image>
-# Default UID: 1000 chosen due to popularity on Linux systems. Possibly 501 on MacOS.
-
-echo "Fixing permissions..."
-USER_UID=${USER_UID:-1000}
-USER_GID=${USER_GID:-1000}
-USER=${USERNAME:-app}
-usermod -u "${USER_UID}" "${USER}"
-groupmod -g "${USER_GID}" "${USER}"
-chown --recursive "${USER_UID}:${USER_GID}" "${WORKSPACE_ROOT}"
 
 ### Set the $PUBLIC_KEY env var to enable SSH access.
 # It is useful to have the full SSH server e.g. on Runpod.
@@ -22,17 +13,24 @@ if [[ -n "$SSH_PUB_KEY" ]] && [[ ! -d "${HOME}/.ssh" ]]; then
     mkdir -p "${HOME}/.ssh"
     echo "${SSH_PUB_KEY}" > "${HOME}/.ssh/authorized_keys"
     chmod -R 700 "${HOME}/.ssh"
-    service ssh start
+    sudo service ssh start
 fi
 # start jupyter notebook in background and redirect output to logfile
 # change working directory to workspace root
 # set token to value of JUPYTER_TOKEN
 # set port to value of JUPYTER_DOCKER_PORT
 if [[ -z "$(command -v jupyter)" ]]; then
-    echo "Jupyter not installed. Exiting..."
-    exit 1
+    echo "Jupyter not installed. Installing jupyter..."
+    pip install jupyterlab
+    echo "$PATH"
+    # check if .local/bin is in $PATH, if not add it
+    if ! echo "$PATH" | grep -q '.local/bin'; then
+        echo "Adding .local/bin to PATH"
+        export PATH="$PATH:$HOME/.local/bin"
+    fi
 fi
-exec gosu "${USER}" jupyter lab \
+export PATH="$PATH:$HOME/.local/bin"
+exec jupyter lab \
     --no-browser \
     --notebook-dir="$WORKSPACE_ROOT" \
     --ServerApp.token="$JUPYTER_TOKEN" \
